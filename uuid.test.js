0841765b241ab5c4652606dc5fd04344ff72d7e9
Taisuke Fukuno <fukuno@jig.jp>
2025-12-23 10:43:06 +0900

    Delete uuid.test.js

--- uuid.test.js
+++
@@ -1,9 +0,0 @@
-import * as t from "https://deno.land/std/testing/asserts.ts";
-import { UUID } from "./UUID.js";
-
-Deno.test("simple", () => {
-  const chk = UUID.create();
-  const sample = "b9d3b3b4-7547-402a-ad57-0337e0e6ff36";
-  t.assertEquals(chk.length, sample.length);
-  t.assertEquals(chk.indexOf("="), sample.indexOf("="));
-});
